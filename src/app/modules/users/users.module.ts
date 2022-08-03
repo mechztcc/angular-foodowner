7f99a7dff64eb7e5ee455256d4fbed4a81f35d7b
mechztcc <dojathan@gmail.com>
2022-08-03 17:20:26 -0400

    feat: add login / create account page

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
 import { CardCreateAccountComponent } from './components/card-create-account/card-create-account.component';
+import { CardLoginComponent } from './components/card-login/card-login.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
     CreateAccountComponent,
-    CardCreateAccountComponent
+    CardCreateAccountComponent,
+    CardLoginComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,

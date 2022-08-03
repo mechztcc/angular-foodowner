d7072601a24fada0c8fc089a1742b8b5f99c5c29
mechztcc <dojathan@gmail.com>
2022-08-03 17:38:47 -0400

    feat: delete some content created by theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppRoutingModule } from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
-import { HomeModule } from './home/home.module';
-import { LoginComponent } from './login/login.component';
-import { UsersModule } from './modules/users/users.module';
-import { CoreModule } from './modules/core/core.module';
+import { CoreModule } from "./modules/core/core.module";
+import { UsersModule } from "./modules/users/users.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LandingComponent,
-    ProfileComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -34,11 +24,10 @@ import { CoreModule } from './modules/core/core.module';
     FormsModule,
     RouterModule,
     AppRoutingModule,
-    HomeModule,
     UsersModule,
-    CoreModule
+    CoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
   {
     path: "users",
     loadChildren: () =>

d6fb8758108152d37b039637c5c29ee26c78793a
mechztcc <dojathan@gmail.com>
2022-08-04 09:39:37 -0400

    feat: add navbar

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,18 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    FooterComponent
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [FooterComponent, HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}

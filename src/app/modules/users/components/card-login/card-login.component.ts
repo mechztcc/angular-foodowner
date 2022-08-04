75366c367f7eda31b296e21d8acb4f2a7e34857f
mechztcc <dojathan@gmail.com>
2022-08-04 08:40:38 -0400

    feat: added login

--- src/app/modules/users/components/card-login/card-login.component.ts
+++ src/app/modules/users/components/card-login/card-login.component.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { Login } from "../../shared/types/login.interface";
-import { User } from "../../shared/types/user.interface";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UsersService } from '../../shared/services/users.service';
+import { Login } from '../../shared/types/login.interface';
+import { User } from '../../shared/types/user.interface';
 
 @Component({
-  selector: "card-login",
-  templateUrl: "./card-login.component.html",
-  styleUrls: ["./card-login.component.css"],
+  selector: 'card-login',
+  templateUrl: './card-login.component.html',
+  styleUrls: ['./card-login.component.css'],
 })
 export class CardLoginComponent implements OnInit {
   isLoading: boolean = false;
   form: FormGroup;
   login: Login;
 
-  errors: boolean[] = [];
+  submited: boolean = false;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private toastrService: ToastrService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit(): void {
     this.initForm();
@@ -23,18 +31,40 @@ export class CardLoginComponent implements OnInit {
 
   initForm() {
     this.form = this.fb.group({
-      email: ["", Validators.compose([Validators.required, Validators.email])],
-      password: ["", Validators.compose([Validators.required, Validators.minLength(6)])],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
     });
   }
 
   validateForm() {
     if (!this.form.invalid) {
+      this.submited = false;
       this.login = {
-        email: this.form.controls["email"].value,
-        password: this.form.controls["password"].value,
+        email: this.form.controls['email'].value,
+        password: this.form.controls['password'].value,
       };
       console.log(this.login);
+      this.createSession();
     }
+    if (this.form.invalid) {
+      this.submited = true;
+      this.toastrService.error('Formulário incorreto', 'Error');
+    }
+  }
+
+  createSession() {
+    this.isLoading = true;
+    this.usersService
+      .login(this.login)
+      .subscribe((data: any) => {
+        console.log(data);
+        this.toastrService.success('Logado com sucesso!', 'Sucesso');
+      })
+      .add(() => {
+        this.isLoading = false;
+      });
   }
 }

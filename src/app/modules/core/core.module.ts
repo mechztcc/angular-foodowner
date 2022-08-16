96cc87bb02773bab205886d0736b7485541a6904
mechztcc <dojathan@gmail.com>
2022-08-16 10:21:54 -0400

    feat: change home page and app bar

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AppBarComponent } from './components/app-bar/app-bar.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent, SidenavComponent],
+  declarations: [FooterComponent, HeaderComponent, SidenavComponent, AppBarComponent],
   imports: [CommonModule],
   exports: [FooterComponent, HeaderComponent, SidenavComponent],
 })
